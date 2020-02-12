7be909e78e5d9e66d38d4a3899cced1dbf6dbb93
Dirk-Jan de Beijer <ddebeijer@outlook.com>
2020-02-12 13:17:25 +0100

    declarations

--- src/app/modules/company-dashboard/company-dashboard.module.ts
+++ src/app/modules/company-dashboard/company-dashboard.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { CompanyDashboardComponent } from './pages/company-dashboard/company-dashboard.component';
 import { CompanyDashboardRoutingModule } from './company-dashboard.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { BalanceComponent } from './pages/company-dashboard/balance/balance.component';
+import { ConsumptionComponent } from './pages/company-dashboard/consumption/consumption.component';
+import { ProductionComponent } from './pages/company-dashboard/production/production.component';
 
 
 
 @NgModule({
-  declarations: [CompanyDashboardComponent],
+  declarations: [
+    CompanyDashboardComponent, 
+    BalanceComponent,
+    ConsumptionComponent,
+    ProductionComponent
+  ],
   imports: [
     CommonModule,
     CompanyDashboardRoutingModule,
