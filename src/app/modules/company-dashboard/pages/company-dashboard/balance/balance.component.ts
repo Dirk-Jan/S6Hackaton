2291c744d83bdd8b2effcbe75abe866380357c50
Dirk-Jan de Beijer <ddebeijer@outlook.com>
2020-02-12 15:15:52 +0100

    demo data gefikst

--- src/app/modules/company-dashboard/pages/company-dashboard/balance/balance.component.ts
+++ src/app/modules/company-dashboard/pages/company-dashboard/balance/balance.component.ts
@@ -10,21 +10,21 @@ export class BalanceComponent implements OnInit {
   public chartType: string = 'line';
 
   public chartDatasets: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+    { data: [85, 69, 65, 51, 56, 55, 62, 70, 68, 74, 84, 87], label: 'Produced energy' },
+    { data: [80, 66, 60, 49, 54, 53, 58, 68, 66, 73, 80, 84], label: 'Consumed energy' }
   ];
 
-  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'December'];
 
   public chartColors: Array<any> = [
     {
-      backgroundColor: 'rgba(105, 0, 132, .2)',
-      borderColor: 'rgba(200, 99, 132, .7)',
+      backgroundColor: 'rgba(46, 204, 113, .2)',
+      borderColor: 'rgba(39, 174, 96, .7)',
       borderWidth: 2,
     },
     {
-      backgroundColor: 'rgba(0, 137, 132, .2)',
-      borderColor: 'rgba(0, 10, 130, .7)',
+      backgroundColor: 'rgba(231, 76, 60, .2)',
+      borderColor: 'rgba(192, 57, 43, .7)',
       borderWidth: 2,
     }
   ];
