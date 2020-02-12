648ed751d66894c8b9e11dfc96d249f982bbe907
Dirk-Jan de Beijer <ddebeijer@outlook.com>
2020-02-12 14:44:54 +0100

    Alleen grafiek van balans zichtbaar

--- src/app/modules/company-dashboard/pages/company-dashboard/consumption/consumption.component.ts
+++ src/app/modules/company-dashboard/pages/company-dashboard/consumption/consumption.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consumption.component.css']
 })
 export class ConsumptionComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
