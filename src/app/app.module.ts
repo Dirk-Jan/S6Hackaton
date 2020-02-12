4fd2601bab0fa9fd02702620eaa33064f8c02912
Furkan <furkan.demirci@live.nl>
2020-02-12 10:08:07 +0100

    Added sidenav structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { NavComponent } from './layouts/nav/nav.component';
 import { ContentLayoutComponent } from './layouts/content-layout/content-layout.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    SideNavComponent,
     ContentLayoutComponent,
   ],
   imports: [
