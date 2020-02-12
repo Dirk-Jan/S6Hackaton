017a9160679545ecad39b58e45055f4c2a32086e
Daphne van de Laar <daphnevandelaar@live.nl>
2020-02-12 11:17:08 +0100

    updated consumer and added a new model

--- src/app/shared/models/consumer.model.ts
+++ src/app/shared/models/consumer.model.ts
@@ -1,6 +1,8 @@
 import { Place } from './place.model';
+import { ElectricityPerDay } from './electricityPerDay.model';
 
 export interface Consumer {
     electricityPrice: string; 
     place: Place;
+    electricityConsumedPerDay: ElectricityPerDay[];
 }
